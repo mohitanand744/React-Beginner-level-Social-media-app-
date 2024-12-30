ae3117840d0c90dc3293f68726fde81307f023de
mohitanand744 <jhamohit744@gmail.com>
2024-12-30 20:38:21 +0530

    resolved some UI Issues

--- src/components/ViewPost.jsx
+++ src/components/ViewPost.jsx
@@ -44,21 +44,23 @@ const ViewPost = () => {
 
   return (
     <div
+      onClick={() => setViewPost(null)}
       className={`flex justify-center items-center bg-[#0000004d] fixed rounded-2xl z-30 ${
         viewPost ? "top-[4.5rem] opacity-100" : "top-[-100%] opacity-50"
       } w-[100%] h-[100vh] transition-all duration-[300ms] ease-in-out overflow-hidden`}
     >
       <div
         onClick={() => setViewPost(null)}
-        className="absolute top-28 right-10 cursor-pointer bg-[#A9DEF9] p-1 rounded-full  border-white border-2"
+        className="absolute top-28 right-10  cursor-pointer bg-[#A9DEF9] p-2 rounded-full  border-white border-2"
       >
         <img
-          className="w-[2.3rem] "
+          className="w-[2rem] "
           src="https://img.icons8.com/ios-glyphs/30/delete-sign.png"
           alt="close-window"
         />
       </div>
       <div
+        onClick={(e) => e.stopPropagation()}
         className={`viewPost flex justify-between bg-white w-[90%] md:w-[65%] lg:w-[85%] xl:w-[73%]  h-[75.7vh] md:h-[82.7vh] rounded-2xl`}
       >
         <div className="hidden lg:block w-[60%] border-r">
@@ -71,16 +73,16 @@ const ViewPost = () => {
           </div>
         </div>
         <div className="comments flex justify-between flex-col   w-[100%] lg:w-[40%]">
-          <div className="top flex justify-between  items-center px-4 py-3">
-            <div className="postDetails flex gap-4 items-center">
+          <div className="flex items-center justify-between px-4 py-3 top">
+            <div className="flex items-center gap-4 postDetails">
               <img
                 className="rounded-full object-cover w-[5rem] h-[5rem] "
                 src={user?.profileImage}
                 alt=""
               />
               <div className="txt ">
-                <p className="font-bold text-2xl">{viewPost?.username}</p>
-                <p className="text-xl text-gray-500 font-semibold">
+                <p className="text-2xl font-bold">{viewPost?.username}</p>
+                <p className="text-xl font-semibold text-gray-500">
                   {viewPost?.caption}
                 </p>
               </div>
@@ -91,7 +93,7 @@ const ViewPost = () => {
             {viewPost?.comments.map((comment) => (
               <div
                 key={comment.commentId}
-                className="comment flex items-center justify-between"
+                className="flex items-center justify-between comment"
               >
                 <div className="flex gap-3">
                   <div className="proImg">
@@ -101,14 +103,14 @@ const ViewPost = () => {
                       alt=""
                     />
                   </div>
-                  <div className="txt flex flex-col gap-2">
+                  <div className="flex flex-col gap-2 txt">
                     <div className="flex gap-3">
                       <h3 className="text-2xl fw-semibold ">
                         {comment.username}
                       </h3>
                       <p className="text-2xl">{comment.commentText}</p>
                     </div>
-                    <div className="flex gap-3 text-xl fw-medium text-gray-700">
+                    <div className="flex gap-3 text-xl text-gray-700 fw-medium">
                       <p>{formatTimestamp(comment.timestamp)}</p>{" "}
                       <p className="cursor-pointer">1 like</p>{" "}
                       <p className="cursor-pointer">Reply</p>
@@ -144,9 +146,9 @@ const ViewPost = () => {
               alt=""
             />
           </div>
-          <div className="bottom px-4 py-4">
+          <div className="px-4 py-4 bottom">
             <div className="flex justify-between ">
-              <div className="iconsLeft flex gap-3">
+              <div className="flex gap-3 iconsLeft">
                 {isLiked ? (
                   <i
                     onClick={() => setIsLiked(!isLiked)}
@@ -178,7 +180,7 @@ const ViewPost = () => {
             </div>
             <p className="text-2xl font-semibold">{likes}</p>
             <div className="mt-2 mb-5">
-              <p className="caption font-medium text-2xl">
+              <p className="text-2xl font-medium caption">
                 {viewPost?.caption}...more
               </p>
             </div>
